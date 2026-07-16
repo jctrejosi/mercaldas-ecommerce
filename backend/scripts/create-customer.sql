a6db2db84bb73aabe63fb3fd789507df77b46860
Juan Trejos <juanktrejos@gmail.com>
2026-07-16 16:13:19 -0500

    create customer user

--- backend/scripts/create-customer.sql
+++ backend/scripts/create-customer.sql
@@ -10,17 +10,6 @@ INSERT INTO customers (
     updated_at
 ) VALUES
 (
-    'juan@ejemplo.com',
-    '$2a$10$N9qo8uLOickgx2ZMRZoMy.Mr/.x6g8kCg.oAeFvk4QoM7.m7yLqZq',
-    'Juan',
-    'Pérez',
-    '3001234567',
-    'registered',
-    true,
-    NOW(),
-    NOW()
-),
-(
     'maria@ejemplo.com',
     '$2b$10$ASps5qrnwl0GwJ5pVgffYOKzyukXgffym6jONm6Ue6TvCWbfSjsDC',
     'María',
