273e974ac73605fde7e88b191e5afb6510997e05
Juan Trejos <juanktrejos@gmail.com>
2026-07-24 17:07:04 -0500

    fix use apikey

--- backend/src/config/wompi.config.ts
+++ backend/src/config/wompi.config.ts
@@ -1,9 +1,14 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('wompi', () => ({
-  publicKey: process.env.WOMPI_PUBLIC_KEY ?? '',
-  privateKey: process.env.WOMPI_PRIVATE_KEY ?? '',
-  integrityKey: process.env.WOMPI_INTEGRITY_KEY ?? '',
-  apiUrl: process.env.WOMPI_API_URL ?? (process.env.NODE_ENV === 'production' ? 'https://api.wompi.co/v1' : 'https://sandbox.wompi.co/v1'),
-  redirectUrl: process.env.WOMPI_REDIRECT_URL ?? '',
-}));
+export default registerAs('wompi', () => {
+  const publicKey = process.env.WOMPI_PUBLIC_KEY ?? '';
+  const isSandbox = publicKey.startsWith('pub_stag_');
+
+  return {
+    publicKey,
+    privateKey: process.env.WOMPI_PRIVATE_KEY ?? '',
+    integrityKey: process.env.WOMPI_INTEGRITY_KEY ?? '',
+    apiUrl: process.env.WOMPI_API_URL ?? (isSandbox ? 'https://sandbox.wompi.co/v1' : 'https://api.wompi.co/v1'),
+    redirectUrl: process.env.WOMPI_REDIRECT_URL ?? '',
+  };
+});
